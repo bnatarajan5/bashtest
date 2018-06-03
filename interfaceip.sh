cf3b6e67e780e0eebadbf0ff9966e165d33d6f0f
Balasubramaniam Natarajan <bnatarajan5@gmail.com>
2018-06-03 17:30:31 +0530

    Added Netstat command

--- interfaceip.sh
+++ interfaceip.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Author: Balasubramaniam Natarajan
 #Date: 3Jun2018
-#Description: This coude will run ifconfig and show the IP an instant to read form.
+#Description: This code will run ifconfig and show the IP an instant to read form. 
+# As well as show the Established connections on a machine via netstat. 
 
 /sbin/ifconfig | /bin/grep -B 1 "inet addr:" | /bin/sed ':a;N;$!ba;s/\n//g' \
 | /bin/sed "s/--/\n/g" | /bin/egrep -o "^\w+.*inet\saddr:\w+\.\w+\.\w+\.\w+" \
 | /bin/sed "s/Link.*inet addr:/ interface has the IP = /g" | sed "s/  //g"
+
+/bin/netstat -ant | /bin/grep "ESTAB"
