f93bf6ff9050d7ca65fa1eec7747405d270ee480
Balasubramaniam Natarajan <bnatarajan5@gmail.com>
2018-06-03 16:02:06 +0530

    First Commit - Working on Ubuntu16.04

---
+++ interfaceip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author: Balasubramaniam Natarajan
+#Date: 3Jun2018
+#Description: This coude will run ifconfig and show the IP an instant read form.
+
+/sbin/ifconfig | /bin/grep -B 1 "inet addr:" | /bin/sed ':a;N;$!ba;s/\n//g' \
+| /bin/sed "s/--/\n/g" | /bin/egrep -o "^\w+.*inet\saddr:\w+\.\w+\.\w+\.\w+" \
+| /bin/sed "s/Link.*inet addr:/ interface has the IP = /g" | sed "s/  //g"
