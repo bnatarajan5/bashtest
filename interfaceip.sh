1738cf62e7608c49481131ed3bd191047e8f32ba
Balasubramaniam Natarajan <bnatarajan5@gmail.com>
2018-06-03 16:24:04 +0530

    Typo in interfaceip.sh

--- interfaceip.sh
+++ interfaceip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Author: Balasubramaniam Natarajan
 #Date: 3Jun2018
-#Description: This coude will run ifconfig and show the IP an instant read form.
+#Description: This coude will run ifconfig and show the IP an instant to read form.
 
 /sbin/ifconfig | /bin/grep -B 1 "inet addr:" | /bin/sed ':a;N;$!ba;s/\n//g' \
 | /bin/sed "s/--/\n/g" | /bin/egrep -o "^\w+.*inet\saddr:\w+\.\w+\.\w+\.\w+" \
